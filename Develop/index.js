58781afe3f7697fbb6172cbe2123bbe059693405
cpapanicholas <cpapanicholas@gmail.com>
2023-10-12 18:26:51 -0500

    added more code to questions section

--- Develop/index.js
+++ Develop/index.js
@@ -13,34 +13,44 @@ const questions = [
     {
         type: 'input',
         name: 'title',
+        message: 'Enter project title:',
     },
     {
         type: 'input',
         name: 'description',
+        message: 'Provide an ample description of your project:',
+
     },
     {
         type: 'input',
         name: 'installation',
+        message: 'Explain how to install your project:'
     },
     {
         type: 'input',
         name: 'usage',
+        message: 'Provide clear instructions on how to use your project:',
     },
     {
         type: 'input',
         name: 'contributions',
+        message: 'Explain how others can contribute:',
     },
     {
         type: 'input',
         name: 'license',
+        message: 'Choose a license for your project',
+        choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3-Clause', 'Unlicense', 'Other'],
     },
     {
         type: 'input',
         name: 'githubUsername',
+        message: 'Enter your GitHub username:',
     },
     {
         type: 'input',
         name: 'email',
+        message: 'Enter your email address:'
     },
     {
         type: 'input',
